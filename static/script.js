dfc84d762f9c7fe66a6888025759c1b47eb481d5
DOUGLAS FRANCISCO DA SILVA <149700231+Dougas85@users.noreply.github.com>
2025-04-14 14:46:30 -0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,3 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navbarText = document.getElementById('navbar-text');
+    navbarText.style.animation = 'grow 2s forwards, blink 1s 3';
+});
 document.getElementById('showResults').addEventListener('click', function() {
     document.getElementById('results').style.display = 'block';
     document.getElementById('errors').style.display = 'none';
@@ -7,4 +11,6 @@ document.getElementById('showErrors').addEventListener('click', function() {
     document.getElementById('errors').style.display = 'block';
     document.getElementById('results').style.display = 'none';
 });
+
+
 
