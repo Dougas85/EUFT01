58a3737678e3024344e4d349d80b5dcdba178bf6
DOUGLAS FRANCISCO DA SILVA <149700231+Dougas85@users.noreply.github.com>
2025-06-11 08:07:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,16 @@ from datetime import datetime
 import tempfile
 from flask import send_file
 from flask import Flask, request, render_template, redirect, flash, url_for, session
-from placas import placas_scudo2, placas_scudo7, placas_analisadas2, placas_analisadas7, placas_especificas2, placas_especificas7, placas_mobi2, placas_mobi7, placas_to_lotacao2, placas_to_lotacao7,
+from placas import (
+    placas_scudo2, placas_scudo7,
+    placas_analisadas2, placas_analisadas7,
+    placas_especificas2, placas_especificas7,
+    placas_mobi2, placas_mobi7,
+    placas_to_lotacao2, placas_to_lotacao7,
     placas_scudo1, placas_mobi1,
-    placas_especificas1, placas_analisadas1, placas_to_lotacao1
+    placas_especificas1, placas_analisadas1,
+    placas_to_lotacao1,
+)
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
